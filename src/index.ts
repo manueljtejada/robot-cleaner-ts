867d0c64fa966a6ae74a2b0c41204ca126d8ed25
Manuel Tejada <manute32@gmail.com>
2020-03-16 22:13:05 +0100

    Improve documentation

--- src/index.ts
+++ src/index.ts
@@ -7,10 +7,11 @@ const rl = readline.createInterface({
 });
 
 /**
- *
- * @param question
+ * Prompts user input with
+ * @param {string} question
+ * @returns {Promise<string>} promise with user's response
  */
-function prompt(question: string) {
+function prompt(question: string): Promise<string> {
   return new Promise<string>(resolve => {
     rl.question(`${question} `, (answer: string) => {
       resolve(answer);
@@ -19,56 +20,47 @@ function prompt(question: string) {
 }
 
 /**
- *
+ * Parse coordinate input string and returns array of coordinates [x, y]
+ * @param {string} str
  */
-const getNumberOfCommands = () => {
-  return prompt('How many commands should the robot expect to execute?');
+function parseCoordinates(str: string): number[] {
+  return str.split(' ').map(coordinate => parseInt(coordinate, 10));
 }
 
 /**
- *
+ * Parse command input string and returns list of commands
+ * @param {string} str
  */
-const getStartingCoordinates = () => {
-  return prompt('Please enter the robot\'s starting coordinates separated by a space (e.g., 10 20)');
+function parseCommands(str: string) {
+  return str.split(', ').map(cmd => {
+    const [direction, steps] = cmd.split(' ');
+
+    return {
+      direction,
+      steps: parseInt(steps, 10),
+    }
+  });
 }
 
 /**
- *
- */
-const getCommands = () => {
-  return prompt('Please enter the commands the robot should follow indicating the direction and the direction separated by a space. To add multiple commands, separate them with a comma (e.g., "E 2, N 1" )');
-}
-
-function parseCoordinates(coordinate: string) {
-  return parseInt(coordinate, 10);
-}
-
-function parseCommands(command: string): Command {
-  const [direction, steps] = command.split(' ');
-
-  return {
-    direction,
-    steps: parseInt(steps, 10),
-  }
-}
-
-/**
- *
+ * Main function
  */
 const main = async () => {
-  const numberOfCommands = await getNumberOfCommands();
-  const startingCoordinates = await getStartingCoordinates();
-  const commandList = await getCommands();
+  // Get user input
+  const numberOfCommands = await prompt('How many commands should the robot expect to execute?');
+  const startingCoordinates = await prompt('Please enter the robot\'s starting coordinates separated by a space (e.g., 10 20)');
+  const commandList = await prompt('Please enter the commands the robot should follow indicating the direction and the direction separated by a space. To add multiple commands, separate them with a comma (e.g., "E 2, N 1")');
 
-  const [x, y] = startingCoordinates.split(' ').map(parseCoordinates);
-  const commands = commandList.split(', ').map(parseCommands);
+  // Parse user input
+  const [x, y] = parseCoordinates(startingCoordinates);
+  const commands = parseCommands(commandList);
 
+  // Create new robot instance and call `start` method to get result
   const robot = new Robot(x, y, commands);
-
   const result = robot.start();
 
+  // Log result and close readline interface
   console.log(`=> Cleaned: ${result}`);
-
   rl.close()
 }
 
