0f8a6348586a80d97f6e8a81d24d1ef54d24c810
Manuel Tejada <manute32@gmail.com>
2020-03-16 21:51:14 +0100

    Improve Robot class documentation

--- src/robot.ts
+++ src/robot.ts
@@ -1,19 +1,28 @@
-export interface CleanedSpot {
+/** Interface representing a cleaned spot */
+interface CleanedSpot {
   x: number;
   y: number;
 }
 
+/** Interface representing a robot's command */
 export interface Command {
   direction: string;
   steps: number;
 }
 
+/** Class representing a cleaning robot */
 export default class Robot {
   private _x: number;
   private _y: number;
   private _cleanedSpots: CleanedSpot[];
-  private _commands: Command[];
+  private readonly _commands: Command[];
 
+  /**
+   * Create a robot
+   * @param {number} x
+   * @param {number} y
+   * @param {Command[]} commands
+   */
   constructor(x: number, y: number, commands: Command[]) {
     this._x = x;
     this._y = y;
@@ -21,15 +30,25 @@ export default class Robot {
     this._cleanedSpots = [{ x, y }];
   }
 
-  start() {
+  /**
+   * Starts cleaning based on given commands
+   * @returns {number} the total number of spots cleaned
+   */
+  public start(): number {
     for (const command of this._commands) {
-      this.move(command);
+      this.executeCommand(command);
     }
 
     return this._cleanedSpots.length;
   }
 
-  move({ direction, steps }: Command) {
+  /**
+   * Executes a given command
+   * @param {Command} command
+   */
+  private executeCommand(command: Command) {
+    const { direction, steps } = command;
+
     let stepsLeft = steps;
     let currentStep = 1;
 
@@ -54,24 +73,36 @@ export default class Robot {
         default:
           break;
       }
+
       currentStep += 1;
       stepsLeft -= 1;
     }
   }
 
-  clean(x: number, y: number) {
+  /**
+   * Move the robot's position
+   * @param {number} x
+   * @param {number} y
+   */
+  private move(x: number, y: number) {
     this._x = x;
     this._y = y;
+  }
+
+  /**
+   * Mark a spot as cleaned if it has not been cleaned yet
+   * @param {number} x
+   * @param {number} y
+   */
+  private clean(x: number, y: number) {
+    this.move(x, y);
 
     if (this._cleanedSpots) {
       const alreadyCleaned = this._cleanedSpots.find(spot => spot.x === x && spot.y === y);
       if (alreadyCleaned) return;
     }
 
-    const newSpot: CleanedSpot = {
-      x,
-      y,
-    }
+    const newSpot: CleanedSpot = { x, y };
 
     this._cleanedSpots = [
       ...this._cleanedSpots,
